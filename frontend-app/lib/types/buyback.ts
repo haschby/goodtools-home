49b9335cef89b4807f701797941d46d54bf4494b
axelSchouten <axel.schouten@chargeguru.com>
2026-07-28 20:26:56 +0200

    Remore git restriction for lib folder

---
+++ frontend-app/lib/types/buyback.ts
@@ -0,0 +1,20 @@
+class EnumBuybackStatus {
+    static readonly TO_BE_TREATED = 'A Traiter';
+    static readonly VALIDATED = 'Valider';
+}
+
+interface DocumentStorage {
+    storage_key: string;
+    url: string;
+}
+
+export interface Buyback {
+    id: string;
+    status: EnumBuybackStatus;
+    gc_booking: string;
+    file_path?: string;
+    amount?: number;
+    created_at: string;
+    updated_at: string;
+    document?: DocumentStorage;
+}
